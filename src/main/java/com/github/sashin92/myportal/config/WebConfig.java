a04f78c4fdcb62c4eb2084d4e44b0412808c90d8
aopao <aopao@ntab>
2024-07-07 19:43:49 +0900

    update interceptor

--- src/main/java/com/github/sashin92/myportal/config/WebConfig.java
+++ src/main/java/com/github/sashin92/myportal/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.github.sashin92.myportal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.github.sashin92.myportal.interceptor.AuthInterceptor;
+import com.github.sashin92.myportal.service.UserService;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+	@Autowired
+	private UserService userService;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
@@ -14,5 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
 			.addResourceLocations("file:c:/tmp/");
 	}
 
-	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuthInterceptor(userService))
+			.addPathPatterns("/board/**")
+			.excludePathPatterns("/board")
+			.excludePathPatterns("/board/")
+			.excludePathPatterns("/board/list");
+	}
 }
